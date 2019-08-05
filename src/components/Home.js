9e1a8f95e4eb7c5bfff2e205afa0dffa1dff3fb9
Desi Steib <dsteib11@gmail.com>
2019-08-05 15:09:16 -0400

    refactoring width issue with products and details page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,30 +7,28 @@ export default class Home extends Component {
 		return (
 			<section class="showcase">
 				<div className="showcase-content">
-					<div className="container">
-						<MainTitle>
-							<div className="col-10 mx-auto text-center home-title">
-								<h1 className="text-capitalize font-weight-bold title">
-									ready settt <strong>cook</strong>
-								</h1>
-							</div>
-						</MainTitle>
-						<div className="showcase-info">
-							<h2>about the collection</h2>
-							<p>
-								Ready Settt Cook is our collection of the very best basics— well-curated, high-quality,
-								exceptionally affordable pieces that allow anyone to build a beautiful kitchen set at a
-								price that’s accessible to all.
-							</p>
-							<Link to="/products">
-								<HomeBtn>
-									<button class="btn btn-xl px-5 py-3 text-uppercase">
-										let's shop
-										<i classNameName="fas fa-chevron-right btn-icon px-3" />
-									</button>
-								</HomeBtn>
-							</Link>
+					<MainTitle>
+						<div className="col-10 mx-auto text-center home-title">
+							<h1 className="text-capitalize font-weight-bold title">
+								ready settt <strong>cook</strong>
+							</h1>
 						</div>
+					</MainTitle>
+					<div className="showcase-info">
+						<h2>about the collection</h2>
+						<p>
+							Ready Settt Cook is our collection of the very best basics— well-curated, high-quality,
+							exceptionally affordable pieces that allow anyone to build a beautiful kitchen sets at a
+							price that’s accessible to all.
+						</p>
+						<Link to="/products">
+							<HomeBtn>
+								<button class="btn btn-xl px-5 py-3 text-uppercase">
+									let's shop
+									<i classNameName="fas fa-chevron-right btn-icon px-3" />
+								</button>
+							</HomeBtn>
+						</Link>
 					</div>
 				</div>
 				{/* <Title name="readysettt" title="cook" />
@@ -61,6 +59,7 @@ const MainTitle = styled.div`
 		color: var(--mainWhite);
 	}
 	.title strong {
-		border-bottom: .2rem solid var(--mainDark);
+		color: var(--darkMaginta);
+		/* border-bottom: .2rem solid var(--mainDark); */
 	}
 `;
--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export const storeProducts = [
 		price: 90,
 		company: 'All-Clad',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'Aluminum body with non-stick coating for easy cooking and cleaning--BPA-free.Comfortable soft- touch handles stay cool during use, vented glass lids let steam escape. Spiral bottom heats evenly, compatible with gas, electric, and glass stovetops(note: not induction compatible). Hand wash only; not dishwasher safe',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -18,7 +18,7 @@ export const storeProducts = [
 		price: 1600,
 		company: 'Williams-Sonoma',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'Built to Stand the Test of Time: 5-ply and fully clad, with an aluminum core surrounded by 18/10 stainless steel. Meticulously Designed: From opting for riveting over welding,to the shape, material, and size of the handles, every element is thoughtfully constructed',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -30,7 +30,7 @@ export const storeProducts = [
 		price: 2000,
 		company: 'Williams-Sonoma',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'tainless steel skillet set with lids – includes a complete suite of fry pans, stock pots, sauce pans, pans, utensils and more. Induction Ready- This Top Of The Line All Clad Cookware Set Is Suitable For All Cooking Sources Such As Stoves & Cooktops, Electric, Gas, Infrared, Induction Stoves, Glass & Ceramic Cooktops. Safe: BPA Free, Dishwasher & Oven Safe Up To 550 Degrees Fahrenheit.Freezer - Safe For Extensive Refrigerated Food Storage.',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -42,7 +42,7 @@ export const storeProducts = [
 		price: 1500,
 		company: 'Williams-Sonoma',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'Copper and stainless steel cookware. Set includes two saucepans, two frying pans, a stewpan, a saute pan, and a Copperbrill cleaner. Lids for sauce, stew, and saute pans included Faster, more even heating Compatible with gas, electric, and halogen cook tops',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -54,7 +54,7 @@ export const storeProducts = [
 		price: 50,
 		company: 'Unbranded',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'Bundle Package Includes: Cook N Home 12-Piece Nonstick Stay Cool Handle Cookware Set. Add this Nonstick Stay Cool Handle Cookware Set to your cookware collection, it features 1.5 and 2.5 quart saucepans with lids, 3 quart casserole pan with lid, 5 quart Dutch oven/ stockpot with lid, 8 and 9.5 - inch fry pans, and a Nylon spatula and spoon. It is made of thick gauge Aluminum that provides even / quick heat conduction and prevents hot spots.',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -66,7 +66,7 @@ export const storeProducts = [
 		price: 500,
 		company: 'All-Clad',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'HARD ANODIZED CONSTRUCTION for fast, even heating and easy cleanup; stainless-steel bonded Anti-Warp base; optimized for induction. PFOA- FREE scratch resistant nonstick coating for healthier cooking with less fat; secure - fitting lids trap in heat and moisture and allow for checking on food at a glance. STAINLESS STEEL HANDLES contoured for a comfortable hold and permanently secured with stainless - steel rivets.Compatible with all stovetops including induction. OVEN SAFE to 500 degrees F(without lids)',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -78,7 +78,7 @@ export const storeProducts = [
 		price: 1900,
 		company: 'Williams-Sonoma',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'1.5-mm thick copper cookware allows unsurpassed heat conductivity and control. Polished copper exterior, satin finish stainless steel interior & stainless steel handles. Mauviel copper / stainless cookware construction is 90 % copper and 10 % stainless steel. Made in France with a lifetime warranty',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -90,7 +90,7 @@ export const storeProducts = [
 		price: 230,
 		company: 'Kitchenaid',
 		info:
-			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+			'Designed to stand up to rigorous dishwasher use. Heavy- gauge aluminum .INDUCTION - CAPABLE stay - flat base,Three - layer, PFOA - free nonstick coating. Oven safe up to 400 degrees',
 		inCart: false,
 		count: 0,
 		total: 0
@@ -104,7 +104,7 @@ export const detailProduct = {
 	price: 90,
 	company: 'All-Clad',
 	info:
-		'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		'Aluminum body with non-stick coating for easy cooking and cleaning--BPA-free.Comfortable soft- touch handles stay cool during use, vented glass lids let steam escape. Spiral bottom heats evenly, compatible with gas, electric, and glass stovetops(note: not induction compatible). Hand wash only; not dishwasher safe',
 	inCart: false,
 	count: 0,
 	total: 0
